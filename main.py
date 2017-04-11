12a42c2ad893afa0ac46f91aef32e82f32b49d5e
kodyvanry <kody@tealdrones.com>
2017-04-11 12:38:03 -0600

    We'll see how this works #11

--- main.py
+++ main.py
@@ -68,6 +68,7 @@ class Game:
         self.wrong_answers = 0
         self.size = helper.get_screen_size()
         self.blank_image = pygame.image.load("images/num0/0001.png")
+        self.blank_image = pygame.transform.scale(self.blank_image, (helper.get_ff_size(self.blank_image)))
 
     def reset(self):
         self.answer_strings = []
@@ -118,6 +119,11 @@ class Game:
                     #                                helper.get_screen_size()[1] -
                     #                                helper.get_answer_size()[1] * 4 +
                     #                                helper.get_answer_size()[1] * x))
+                else:
+                    display.blit(self.blank_image, (helper.get_answer_size()[0],
+                                                   helper.get_screen_size()[1] -
+                                                   helper.get_answer_size()[1] * 4 +
+                                                   helper.get_answer_size()[1] * (x - 4)))
             else:
                 display.blit(self.blank_image, (helper.get_answer_size()[0],
                                                helper.get_screen_size()[1] -
